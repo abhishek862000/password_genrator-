ea6aee4ecef33bea8233f023667549f92dec9550
mrak862000 <mrak862000@gmail.com>
2024-09-27 18:33:47 +0530

    password genarator

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,130 @@
-import Image from "next/image";
+"use client";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 
-export default function Home() {
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import { ModeToggle } from "./components/ModeToggle";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+interface PasswordGeneratorForm {
+  passwordLength: number;
+  includeLowercase: boolean;
+  includeUppercase: boolean;
+  includeNumbers: boolean;
+  includeSymbols: boolean;
+}
+
+const PasswordGenerator = () => {
+  const { register, handleSubmit } = useForm<PasswordGeneratorForm>();
+  const [generatedPassword, setGeneratedPassword] = useState("");
+
+  const generatePassword = ({
+    passwordLength,
+    includeLowercase,
+    includeUppercase,
+    includeNumbers,
+    includeSymbols,
+  }: PasswordGeneratorForm) => {
+    const LowercaseChars = "qwertyuiopasdfghjklzxcvbnm";
+    const UppercaseChars = "QWERTYUIOPASDFGHJKLZXCVBNM";
+    const NumberChars = "01234567890123456789";
+    const SymbolsChars = "!@#$%^&*()_+-={[]}:<>?";
+
+    let allowedChars = "";
+    allowedChars += includeLowercase ? LowercaseChars : "";
+    allowedChars += includeUppercase ? UppercaseChars : "";
+    allowedChars += includeNumbers ? NumberChars : "";
+    allowedChars += includeSymbols ? SymbolsChars : "";
+
+    let password = "";
+    for (let i = 0; i < passwordLength; i++) {
+      const randomIndex = Math.floor(Math.random() * allowedChars.length);
+      password += allowedChars[randomIndex];
+    }
+
+    setGeneratedPassword(password);
+    if (password.length > 16) {
+      setGeneratedPassword("The length must be < 16");
+    }
+    if (password.length < 6) {
+      setGeneratedPassword("The length must be > 6");
+    }
+    if (allowedChars.length === 0) {
+      setGeneratedPassword("You must select an option");
+    }
+  };
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+    <div>
+      <div className="p-5 flex flex-row-reverse">
+        <ModeToggle />
+      </div>
+      <div className="flex justify-center">
+        <Card className="p-10">
+          <CardHeader>
+            <CardTitle className="text-center">Password Generator</CardTitle>
+          </CardHeader>
+          <form onSubmit={handleSubmit(generatePassword)}>
+            <label className="mr-3 ">Password Length Min-6 Max-16</label>
+            <Input
+              type="number"
+              placeholder="Enter the length"
+              {...register("passwordLength", { value: 12 })}
+              className="bg-slate-200 rounded-md p-2 dark:bg-gray-50 dark:text-gray-900 mt-2 mb-2 "
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+
+            <div className="inline-flex gap-2 ">
+              <input
+                type="checkbox"
+                {...register("includeLowercase", { value: true })}
+              />
+              <label>Include Lowercase </label>
+            </div>
+            <br />
+            <div className="inline-flex gap-2 ">
+              <input
+                type="checkbox"
+                {...register("includeUppercase", { value: true })}
+              />
+              <label>Include Uppercase </label>
+            </div>
+
+            <br />
+            <div className="inline-flex gap-2 ">
+              <input
+                type="checkbox"
+                {...register("includeNumbers", { value: true })}
+              />
+              <label>Include Numbers</label>
+            </div>
+            <br />
+            <div className="inline-flex gap-2 ">
+              <input
+                type="checkbox"
+                {...register("includeSymbols", { value: true })}
+              />
+              <label>Include Symbols</label>
+            </div>
+            <br />
+            <Button type="submit" className="mt-2 mb-2 px-20">
+              Generate Password
+            </Button>
+          </form>
+          <p
+            className={`text-center text-2xl ${
+              generatedPassword.length < 6 || generatedPassword.length > 16
+                ? "text-red-600"
+                : "text-green-600"
+            }`}
           >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+            
+            {generatedPassword}
+          </p>
+        </Card>
+      </div>
     </div>
   );
-}
+};
+
+export default PasswordGenerator;
